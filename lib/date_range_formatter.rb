e4fd971c980ba08589db66853929e7d3de0c0528
Rabea Gleissner <rabeagleissner@gmail.com>
2022-03-22 08:04:13 +0800

    Extract methods to make code more readable

--- lib/date_range_formatter.rb
+++ lib/date_range_formatter.rb
@@ -10,50 +10,97 @@ class DateRangeFormatter
   end
 
   def to_s
-    if @start_date == @end_date
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} to #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time}"
-      elsif @end_time
-        "#{full_start_date} until #{@end_time}"
-      else
-        full_start_date
-      end
-    elsif @start_date.month == @end_date.month && @start_date.year == @end_date.year
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
-      end
-    elsif @start_date.year == @end_date.year
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        @start_date.strftime("#{@start_date.day.ordinalize} %B - ") + @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
-      end
+    if input_range_on_same_day?
+      format_range_for_same_day
+    elsif input_range_in_same_month?
+      format_range_for_same_month
+    elsif input_range_in_same_year?
+      format_range_for_same_year
     else
-      if @start_time && @end_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date} at #{@end_time}"
-      elsif @start_time
-        "#{full_start_date} at #{@start_time} - #{full_end_date}"
-      elsif @end_time
-        "#{full_start_date} - #{full_end_date} at #{@end_time}"
-      else
-        "#{full_start_date} - #{full_end_date}"
-      end
+      format_range_for_different_year
     end
   end
 
   private
+
+  def input_range_on_same_day?
+    @start_date == @end_date
+  end
+
+  def input_range_in_same_month?
+    @start_date.month == @end_date.month && @start_date.year == @end_date.year
+  end
+
+  def input_range_in_same_year?
+    @start_date.year == @end_date.year
+  end
+
+  def no_times_given?
+    !@start_time && !@end_time
+  end
+
+  def format_same_month_range_without_times
+    @start_date.strftime("#{@start_date.day.ordinalize} - #{@end_date.day.ordinalize} %B %Y")
+  end
+
+  def format_same_year_range_without_times
+    @start_date.strftime("#{@start_date.day.ordinalize} %B - ") +
+      @end_date.strftime("#{@end_date.day.ordinalize} %B %Y")
+  end
+
+  def format_dates_only
+    "#{full_start_date} - #{full_end_date}"
+  end
+
+  def format_range_for_same_day
+    if @start_time && @end_time
+      "#{date_at_time(full_start_date, @start_time)} to #{@end_time}"
+    elsif @start_time
+      date_at_time(full_start_date, @start_time)
+    elsif @end_time
+      "#{full_start_date} until #{@end_time}"
+    else
+      full_start_date
+    end
+  end
+
+  def format_range_for_same_month
+    if no_times_given?
+      format_same_month_range_without_times
+    else
+      format_range_with_start_and_end_dates
+    end
+  end
+
+  def format_range_for_same_year
+    if no_times_given?
+      format_same_year_range_without_times
+    else
+      format_range_with_start_and_end_dates
+    end
+  end
+
+  def format_range_for_different_year
+    if no_times_given?
+      format_dates_only
+    else
+      format_range_with_start_and_end_dates
+    end
+  end
+
+  def format_range_with_start_and_end_dates
+    if @start_time && @end_time
+      "#{date_at_time(full_start_date, @start_time)} - #{date_at_time(full_end_date, @end_time)}"
+    elsif @start_time
+      "#{date_at_time(full_start_date, @start_time)} - #{full_end_date}"
+    elsif @end_time
+      "#{full_start_date} - #{full_end_date} at #{@end_time}"
+    end
+  end
+
+  def date_at_time(date, time)
+    "#{date} at #{time}"
+  end
 
   def full_start_date
     @start_date.strftime("#{@start_date.day.ordinalize} %B %Y")
